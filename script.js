a81264dcbe1d15bfc862c9cbd85aec74495d6fb1
Agiri Taofeek <taofeeqomotolani@gmail.com>
2022-06-23 22:42:03 +0100

    commit JS and HTML files

---
+++ script.js
@@ -0,0 +1,4 @@
+"use strict";
+document.write("<h1>My name is Agiri Taofeek Omotolani</h1><br>");
+document.write("<h1>My height is 1.8m</h1> <br>");
+document.write("<h1>My country is Nigeria</h1> </br>");
